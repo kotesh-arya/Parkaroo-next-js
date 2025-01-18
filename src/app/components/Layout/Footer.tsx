44702a18842d7ef88a59280709b95e11df028c8d
kotesh-arya <koteswarraomudila@gmail.com>
2025-01-18 23:25:46 +0530

    CHORES: Content updation

--- src/app/components/Layout/Footer.tsx
+++ src/app/components/Layout/Footer.tsx
@@ -10,8 +10,7 @@ const Footer = () => {
         <div className="row-span-2 sm:col-span-4 col-start-1 col-end-4 sm:col-end-5 flex flex-col items-start ">
           {/* <LogoVPN className="h-8 w-auto mb-6" /> */}
           <p className="mb-4">
-            <strong className="font-medium">LaslesVPN</strong> is a private
-            virtual network that has unique features and has high security.
+            <strong className="font-medium">Parkaroo</strong> is a parking application that has unique features and has high traction.
           </p>
           {/* <div className="flex w-full mt-2 mb-8 -mx-2">
             <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md">
@@ -25,7 +24,7 @@ const Footer = () => {
             </div>
           </div> */}
           <p className="text-gray-400">
-            ©{new Date().getFullYear()} - LaslesVPN
+            ©{new Date().getFullYear()} - Parkaroo
           </p>
         </div>
         <div className=" row-span-2 sm:col-span-2 sm:col-start-7 sm:col-end-9 flex flex-col">
